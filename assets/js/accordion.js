d5e8e44205a13f25ab7adafe30794cb8356f2be5
sonambishnoi550 <bishnoisonam079@gmail.com>
2024-11-07 12:43:02 +0530

    static done

---
+++ assets/js/accordion.js
@@ -0,0 +1,31 @@
+// accordion js 
+document.querySelectorAll('.accordion-item').forEach((accordionItem) => {
+    accordionItem.addEventListener('click', () => {
+        const content = accordionItem.querySelector('.accordion-content');
+        const plusIcon = accordionItem.querySelector('.accordion-icon-plus');
+        const minusIcon = accordionItem.querySelector('.accordion-icon-minus');
+
+        if (accordionItem.classList.contains('active')) {
+            content.style.maxHeight = '0';
+            accordionItem.classList.remove('active');
+        } else {
+            document.querySelectorAll('.accordion-item').forEach((item) => {
+                const otherContent = item.querySelector('.accordion-content');
+                const otherPlusIcon = item.querySelector('.accordion-icon-plus');
+                const otherMinusIcon = item.querySelector('.accordion-icon-minus');
+                if (item !== accordionItem) {
+                    otherContent.style.maxHeight = '0';
+                    item.classList.remove('active');
+                    otherPlusIcon.classList.remove('hidden');
+                    otherMinusIcon.classList.add('hidden');
+                }
+            });
+            content.style.maxHeight = content.scrollHeight + 'px';
+            accordionItem.classList.add('active');
+        }
+        plusIcon.classList.toggle('hidden');
+        minusIcon.classList.toggle('hidden');
+    });
+});
+
+document.getElementById("year").textContent = new Date().getFullYear();
